fac4b1a3954fd145dc2833a8441ae3b972dc23b4
bbredenkamp <bbredenkamp@gmail.com>
2023-08-15 08:29:17 +0000

    CoPilot first commit

--- skills.js
+++ skills.js
@@ -1,3 +1,7 @@
 function calculateNumbers(var1, var2) {
+    console.log('calculateNumbers()');
+    console.log('var1: ' + var1);
+    console.log('var2: ' + var2);
+    
     return var1 + var2;
 }
